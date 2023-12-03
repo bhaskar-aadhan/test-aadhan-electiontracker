17d598ec23a8f89c5381cf655b1b931b88f84adc
bhaskar.guthula <bhaskar.guthula@aadhan.in>
2023-12-03 10:36:11 +0530

    websocket removed loader aadded

---
+++ .wrangler/tmp/pages-E2GmPk/y69cx5l6uem.js
@@ -0,0 +1,35 @@
+// <define:__ROUTES__>
+var define_ROUTES_default = {
+  version: 1,
+  include: ["/*"],
+  exclude: ["/favicon.ico", "/build/*"]
+};
+
+// node_modules/wrangler/templates/pages-dev-pipeline.ts
+import worker from "D:\\Implementation\\Projects\\Aadhan\\Aadhan-election-Tracker\\dev-branch\\Aadhan-LiveResults\\.wrangler\\tmp\\pages-E2GmPk\\functionsWorker-0.39238346785929945.mjs";
+export * from "D:\\Implementation\\Projects\\Aadhan\\Aadhan-election-Tracker\\dev-branch\\Aadhan-LiveResults\\.wrangler\\tmp\\pages-E2GmPk\\functionsWorker-0.39238346785929945.mjs";
+import { isRoutingRuleMatch } from "D:\\Implementation\\Projects\\Aadhan\\Aadhan-election-Tracker\\dev-branch\\Aadhan-LiveResults\\node_modules\\wrangler\\templates\\pages-dev-util.ts";
+var routes = define_ROUTES_default;
+var pages_dev_pipeline_default = {
+  fetch(request, env, context) {
+    const { pathname } = new URL(request.url);
+    for (const exclude of routes.exclude) {
+      if (isRoutingRuleMatch(pathname, exclude)) {
+        return env.ASSETS.fetch(request);
+      }
+    }
+    for (const include of routes.include) {
+      if (isRoutingRuleMatch(pathname, include)) {
+        if (worker.fetch === void 0) {
+          throw new TypeError("Entry point missing `fetch` handler");
+        }
+        return worker.fetch(request, env, context);
+      }
+    }
+    return env.ASSETS.fetch(request);
+  }
+};
+export {
+  pages_dev_pipeline_default as default
+};
+//# sourceMappingURL=y69cx5l6uem.js.map
